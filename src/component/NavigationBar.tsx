05c188c8486b4b4b9e14da93047f3e1ecf6413d9
Yusuf <teajhaney@gmail.com>
2025-05-25 01:10:57 +0100

    refactor: remove default Next.js components and implement custom NavigationBar

---
+++ src/component/NavigationBar.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const NavigationBar = () => {
+  return (
+    <nav className="h-16 shadow border-b border-accent">
+		  <div className="h-full contentMargin flex items-center justify-between">
+			  <h1>Seobinim</h1>
+	  </div>
+    </nav>
+  );
+};
+
+export default NavigationBar;
